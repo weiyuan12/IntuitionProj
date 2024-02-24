06c870e8b0cabad88511bfa3394b30ca0c2a8611
dnyk7 <marimo.is.lost@gmail.com>
2024-02-24 17:17:37 +0800

    e

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    bot.replying_to(message, message.text)
 
 
 @bot.message_handler(commands=['start', 'hello'])
