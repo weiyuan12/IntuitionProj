2d399bcf9254602849fcac3d0bf596ffe1a40ac0
hongjinhao <50015504+hongjinhao@users.noreply.github.com>
2024-02-24 16:46:47 +0800

    test

--- bot.py
+++ bot.py
@@ -6,17 +6,12 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
-
-
-
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
-
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
 bot.infinity_polling()
